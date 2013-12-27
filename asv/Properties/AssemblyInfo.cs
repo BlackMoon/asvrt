b9f000f946060a9566c78dd2893f86a6bb042742
rrv <admin@mail.ru>
2013-12-27 11:13:46 +0300

    Ext.MessageBox.prototype

--- asv/Properties/AssemblyInfo.cs
+++ asv/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("1.2.2485.1625")]
-[assembly: AssemblyFileVersion("1.2.2485.1625")]
+[assembly: AssemblyVersion("1.2.2486.1110")]
+[assembly: AssemblyFileVersion("1.2.2486.1110")]
