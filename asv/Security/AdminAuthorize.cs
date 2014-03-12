c395f2ebab87cda64bd6692c3a545e87dbb73b74
rrv <admin@mail.ru>
2014-03-12 18:22:23 +0300

    AdminAuthorize.cs

---
+++ asv/Security/AdminAuthorize.cs
@@ -0,0 +1,19 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace asv.Security
+{
+    public class AdminAuthorize : AuthorizeAttribute
+    {
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            bool isAutorized = base.AuthorizeCore(httpContext);
+            if (isAutorized)
+            {
+                MemberPrincipal user = (MemberPrincipal)httpContext.User;
+                return user.IsAdmin == 1;
+            }
+            return isAutorized;
+        }
+    }
+}
