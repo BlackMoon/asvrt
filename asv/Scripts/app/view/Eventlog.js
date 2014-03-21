816bfe77c83c385f1bfdc31a326e9186b0f53e2f
rrv <admin@mail.ru>
2014-03-21 20:06:43 +0300

    eventlog

--- asv/Scripts/app/view/Eventlog.js
+++ asv/Scripts/app/view/Eventlog.js
@@ -1,26 +1,31 @@
 ﻿Ext.define('QB.view.Eventlog', {
     extend: 'QB.common.Bargrid',
     alias: 'widget.eventlog',
-    cls: 'log',
-    hideHeaders: true,
+    cls: 'log',    
     enableAdd: false,
     enableEdit: false,
     enableRemove: false,
     enableContext: false,
-    columns: [{ dataIndex: 'event', flex: 1 }],        
-    store: 'Logs',
-    tbarConfig: {
-        kind: 'custom',
-        enableSearch: false,
-        items: [{                
-            text: 'Экспорт',
-            iconCls: 'icon-txt',
-            action: 'export'
-        }]
-    },
+    columns: [ { xtype: 'datecolumn', text: 'Дата', dataIndex: 'dt', format: 'd.m.Y H:i', width: 200 },
+               { text: 'Тип', dataIndex: 'level' },
+               { text: 'Пользователь', dataIndex: 'user' },
+               { text: 'Хост', dataIndex: 'host' },
+               { text: 'Событие', dataIndex: 'message', flex: 1 }],
+    store: 'Logs',    
 
     initComponent: function () {
         var me = this;
+
+        me.tbarConfig = {
+            enable: true,
+            kind: 'custom',
+            enableSearch: false,
+            items: [{                
+                text: 'Экспорт',
+                iconCls: 'icon-txt',
+                action: 'export'
+            }]
+        }
 
         me.callParent(arguments);
         me.store.load();
